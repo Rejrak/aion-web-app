ff6145fb0679614476f9c779d97abd68c1fc488f
Rejrak <lucchettiroberto95@outlook.com>
2025-01-26 20:06:42 +0100

    Update Main Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home'
-import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/Login'
 import { UserProvider } from './context/userContext'
+import DashboardLayoutBasic from './components/commons/Dashboard'
 
 
 function App() {
-  const location = useLocation();
-
-  const homePage = <Home />;
-  const loginPage = <LoginPage onClick={ () => alert("Yeee callback")}/>;
+  const loginPage = <LoginPage/>;
 
   return (
     <UserProvider>
-      {location.pathname !== '/auth' && <NavBar />}
       <Routes>
         <Route path='/auth' element={loginPage} />
-        <Route path='/' element={homePage} />
+        <Route path='/' element={<DashboardLayoutBasic />} />
       </Routes>
     </UserProvider>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import styled, { createGlobalStyle } from 'styled-components';
 import App from './App.tsx';
 
-// Stile globale per l'applicazione (sostituisce index.css)
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: Arial, sans-serif;
-    background-color: #121212;
-    color: #fff;
-    line-height: 1.6;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100%;
-  }
-
-  #root {
-    max-width: 1280px;
-    padding: 2rem;
-    text-align: center;
-  }
-`;
-
-// Contenitore principale per l'app
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-`;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GlobalStyle />
     <BrowserRouter>
-      <MainContainer>
         <App />
-      </MainContainer>
     </BrowserRouter>
   </StrictMode>
 );
